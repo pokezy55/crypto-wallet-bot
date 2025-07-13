eef579b371742beac733a1fd5ee16ba3d2c4991e
kiaa <fasdalimi@gmail.com>
2025-07-13 09:11:05 +0700

    testing

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['t.me', 'cdn4.telegram-cdn.org'],
-  },
-  env: {
-    TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-    TELEGRAM_BOT_USERNAME: process.env.TELEGRAM_BOT_USERNAME,
-    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
   },
 }
 
